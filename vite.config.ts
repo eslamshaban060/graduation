1bf55e69c0fafd9bfff767c2e6a65abda857e56b
eslamshaban060 <eslamshaban060@gmail.com>
2026-02-11 05:00:42 +0200

    try to solve issues3

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(
+//     Boolean,
+//   ),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -17,28 +37,8 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    // إلغاء تحذير حجم الـ chunks
+    chunkSizeWarningLimit: Infinity,
+  },
 }));
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(
-//     Boolean
-//   ),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   build: {
-//     // إلغاء تحذير حجم الـ chunks
-//     chunkSizeWarningLimit: Infinity,
-//   },
-// }));
